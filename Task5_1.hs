f2d32c3f0166b4acd8124d33ce2b2e1c6ccc1be1
Nikita Homenko <44022430+nik27090@users.noreply.github.com>
2020-01-16 19:48:52 +0300

    add task5_1

--- Task5_1.hs
+++ Task5_1.hs
@@ -34,10 +34,31 @@ list2dlist' left (h: t) =
 
 -- Реализуйте функции индексирования, вставки и удаления элементов
 index :: DList a -> Int -> a
-index = todo
+index lst i = index' lst i 0 where
+  index'  DNil              _ _             = error "index :: Out of range"
+  index' (DCons _ curr rhv) i s | s == i    = curr
+                                | otherwise = index' rhv i (s + 1)
 
 insertAt :: DList a -> Int -> a -> DList a
-insertAt list index value = todo
+insertAt list i value = insertAt' list i value 0 where
+  insertAt'  DNil                           0 val _                    = DCons DNil val DNil
+  insertAt' (DCons DNil curr DNil)          i val _   
+                                                    | i == 1 = let rec = DCons DNil curr (DCons rec val DNil)
+                                                               in rec
+                                                    | otherwise        = error "insertAt1 :: Out of range"
+  insertAt' (DCons (DCons l c r) curr DNil) i val sum 
+                                                    | i == sum + 1     = DCons (DCons l c r) curr (DCons r val DNil)
+                                                    | i >  sum + 1     = error "\n insertAt2 :: Out of range" 
+  insertAt' (DCons lhv curr rhv)            i val sum 
+                                                    | i < 0            = error "insertAt3 :: Out of range"
+                                                    | sum == i         = DCons lhv val rhv
+                                                    | otherwise        = DCons lhv curr (insertAt' rhv i val (sum + 1))
 
 removeAt :: DList a -> Int -> DList a
-removeAt list index = todo
+removeAt  DNil                          _           = DNil
+removeAt (DCons lhv curr DNil)          i 
+                                        | i == 0    = DNil
+                                        | otherwise = DCons lhv curr DNil 
+removeAt (DCons lhv curr (DCons l c r)) i 
+                                        | i == 0    = DCons lhv c (removeAt r (i - 1))   
+                                        | otherwise = DCons lhv curr (removeAt (DCons l c r) (i - 1))
